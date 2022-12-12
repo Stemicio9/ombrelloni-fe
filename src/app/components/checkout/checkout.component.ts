fd2bd4203f6aa50c690898dd81437e0f1c5c785e
Stefano Miceli <s.miceli90@gmail.com>
2022-12-12 20:02:47 +0100

    responsive

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/entities/order';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  totale=23.00;
 
+  orderList:Order[] = [
+    new Order('assets/carbonara-image.png','Mezze maniche alla Carbonara',2,18,'assets/trash-icon.png'),
+    new Order('assets/spaghetti-pomodoro.png','Spaghetti al pomodoro',1,5,'assets/trash-icon.png')
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+  formatPrice(p: number){
+    return p.toFixed(2).toString().replace('.',',') + ' €'
   }
 
 }
